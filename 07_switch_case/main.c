f11434c93f360539bdc7749dcf89ed6e3a40bfb6
bongtt0 <hkhjsshj@gmail.com>
2022-09-20 09:03:07 +0900

    2022-09-20

---
+++ 07_switch_case/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*int main()
+{
+    int rank =2, m=0;
+
+    switch(rank)
+    {
+        case 1:
+            m=300;
+            break;
+        case 2:
+            m=200;
+            break;
+        case3:
+            m=200;
+            break;
+        default:
+            m=10;
+            break;
+    }
+
+    printf("a : %d\n", m);
+
+    if(rank==1) m=300;
+    if(rank==2) m=200;
+    if(rank==3) m=100;
+    else m=10;
+
+    printf("a : %d\n", m);
+
+
+    return 0;
+}*/
